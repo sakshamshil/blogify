75a3f722d6123469bd9c9725b90f6286d8279a55
Saksham Shil <sakshamplzcode@gmail.com>
2025-04-29 13:00:20 +0530

    no build

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Make sure we're using the Python from the Vercel environment
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
