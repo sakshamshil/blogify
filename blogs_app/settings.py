5ea5c52e207f4cdb3ee4f3e1fb45ac189ad004df
Saksham Shil <sakshamplzcode@gmail.com>
2025-04-29 13:40:46 +0530

    build.sh

--- blogs_app/settings.py
+++ blogs_app/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')  
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'blog/static'),
+# ]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
